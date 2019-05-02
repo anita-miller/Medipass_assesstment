2235bae77cd962d900fce3f082fc7b59ce69cac6
anita naseri <anita.naseri96@gmail.com>
2019-05-02 16:43:30 +1000

    another file for changing order of pie places

---
+++ src/Parts/ChangeOrderBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+
+const ChangeOrderBar = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default ChangeOrderBar
