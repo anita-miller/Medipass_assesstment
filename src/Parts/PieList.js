f68f094438008329ca2cbd6efab5d5f26e439f4c
anita naseri <anita.naseri96@gmail.com>
2019-05-02 18:43:58 +1000

    adding event handlers changeOrder (when ascending and desc button is clicked) and onSearchChange(when search for specific pie place) and prevPage and nextPage to change pages

--- src/Parts/PieList.js
+++ src/Parts/PieList.js
@@ -1,4 +1,15 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import {user} from '../User';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+
+import Pie from "./Pie"
+import SearchBar from "./SearchBar"
+import Pager from "./Pager"
+import ChangeOrderBar from './ChangeOrderBar';
+
 
 class PieList extends Component{
 
@@ -19,15 +30,52 @@ class PieList extends Component{
   getPies = (pageOffset, order, searchString) => {
   }
 
+  nextPage = () => {
+    this.setState({
+      offset: this.state.offset + 1,
+    });
+    this.getPies(this.state.offset + 1, this.state.order, this.state.searchString);
+  }
+  
+  prevPage = () =>{
+    this.setState({
+      offset: this.state.offset - 1,
+    })
+    this.getPies(this.state.offset - 1, this.state.order, this.state.searchString);
+  }
 
   getStores = () => {
   }
 
   onSearchChange = (e) => {
+    if(e.target.value){
+      this.setState({
+        searchString: e.target.value
+      })
+     } else {
+        this.setState({
+          searchString: ""
+        })
+      }
+    
+    this.getPies(this.state.offset, this.state.order, e.target.value)
   }
 
 
   changeOrder = () => {
+    if(this.state.order === "asc"){
+      let order = "desc";
+      this.setState({
+        order: order,
+      })
+      this.getPies(this.state.offset, order, this.state.searchString)
+    } else {
+      let order = "asc";
+      this.setState({
+        order: order,
+      })
+      this.getPies(this.state.offset, order, this.state.searchString)
+    }
   }
 
   
