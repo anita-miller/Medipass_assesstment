807e36f1e379230855121a6b3208a146e22bcb99
anita naseri <anita.naseri96@gmail.com>
2019-05-02 19:40:54 +1000

    using Card from Material-UI to make cards for each store and pie, price and quantity

--- src/Parts/Pie.js
+++ src/Parts/Pie.js
@@ -7,7 +7,25 @@ import { CardHeader } from '@material-ui/core';
 const Pie = (props) => {
     return(
         <div>  
-            
+            {props.store && props.pie ? (
+                <Card style={{minWidth: 500}}>
+                    <CardHeader 
+                        title = {props.store.displayName}
+                        subheader={`address : ${props.store.address} , phone : ${props.store.mobile}`}  
+                    />
+                    <CardContent>               
+                        <Typography component="p">
+                        {props.pie.displayName} - {props.pie.priceString}
+                        </Typography>
+                        <Typography component="p">
+                            Quantity - {props.pie.quantity}
+                        </Typography>
+                        <Typography component="p">
+                            Rating - {props.store.rating}/10
+                        </Typography>
+                    </CardContent>
+                </Card>
+            ): null}
         </div>
     )
 }
