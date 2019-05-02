eb5311c3117041880d54bc0130bebc2e65e06fa6
anita naseri <anita.naseri96@gmail.com>
2019-05-02 17:14:32 +1000

    add buttons that change the order based on price (will add changeOrder in PieList)

--- src/Parts/ChangeOrderBar.js
+++ src/Parts/ChangeOrderBar.js
@@ -4,7 +4,25 @@ import Button from '@material-ui/core/Button';
 const ChangeOrderBar = (props) => {
     return (
         <div>
-            
+            <Button
+                size="medium"
+                variant="outlined" 
+                onClick={props.changeOrder}
+                style={{ margin: 5 }}
+                color="primary"
+            >
+                Ascending Price
+            </Button>
+
+            <Button
+                size="medium"
+                variant="outlined" 
+                onClick={props.changeOrder}
+                color="primary"
+            >
+                Descending Price
+            </Button>
+
         </div>
     )
 }
