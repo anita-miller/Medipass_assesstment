4d9e27c4fbd2e98f395afb65ca29a06cbe6b9fcd
anita naseri <anita.naseri96@gmail.com>
2019-05-02 16:50:35 +1000

    add Bar on top of the page using Material-UI

---
+++ src/Parts/AppBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+const AppBar = () => {
+    return(
+        <div>
+            <AppBar position="static" style={{ backgroundColor: '#9736D6' }}>
+            
+            </AppBar>
+        </div>
+    )
+}
+export default AppBar;
