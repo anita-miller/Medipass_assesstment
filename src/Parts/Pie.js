a6c42eef90c1afb8d3787f622fd233d1a6bd5197
anita naseri <anita.naseri96@gmail.com>
2019-05-02 16:36:22 +1000

    create Pie.js for each pie create a card using Material-ui

---
+++ src/Parts/Pie.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import { CardHeader } from '@material-ui/core';
+
+const Pie = (props) => {
+    return(
+        <div>  
+            
+        </div>
+    )
+}
+
+export default Pie
